35f52fc8d0088f1cf48ae93706e7c92461d2dc64
Musthaq Ahamad <musthu.gm@gmail.com>
2018-06-22 12:25:33 +0530

    removed path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,5 @@ module.exports = {
     author: 'haxzie',
     profile: 'https://github.com/haxzie'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-  pathPrefix: '/GitMe'
+  plugins: ['gatsby-plugin-react-helmet']
 }
