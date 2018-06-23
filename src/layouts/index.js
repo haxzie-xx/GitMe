5654cec269f1598040f5db9eafd80b5d9579f6ba
Musthaq Ahamad <musthu.gm@gmail.com>
2018-06-23 09:05:26 +0530

    Added favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,8 +16,15 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Make your first GitHub Pull Request' },
         { name: 'keywords', content: 'GitHub, PullRequest, First OpenSource, contribution' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        { name: 'msapplication-TileColor', content: '#603cba'},
+        { name: 'theme-color', content: '#261f28'}
       ]}
     />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ed11ca"/>
     <Header siteTitle={data.site.siteMetadata.title} />
     
     <div className="container">
