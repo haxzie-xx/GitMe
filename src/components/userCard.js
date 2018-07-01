b57b351a3d49dfb911866bb4c27cc094594c8b7b
Musthaq Ahamad <musthu.gm@gmail.com>
2018-07-01 11:12:54 +0530

    Updated user avatar to display 200x200 images

--- src/components/userCard.js
+++ src/components/userCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({fullname, username}) => (
     <a href={"https://github.com/"+username} className="profile">
-        <img src={"https://avatars1.githubusercontent.com/"+username}/>
+        <img src={"https://avatars1.githubusercontent.com/"+username+"?size=200"}/>
         <span>{fullname}</span>
     </a>
 );
