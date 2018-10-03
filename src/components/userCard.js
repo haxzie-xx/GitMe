b69eeaa85899abdf88c302337e2773dd150299cc
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2018-10-02 20:09:15 -0400

    improved how the cards look

--- src/components/userCard.js
+++ src/components/userCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({fullname, username}) => (
     <a href={"https://github.com/"+username} className="profile">
         <img src={"https://avatars1.githubusercontent.com/"+username+"?size=200"}/>
-        <span>{fullname}</span>
+        <p>{fullname}</p>
     </a>
 );
 
